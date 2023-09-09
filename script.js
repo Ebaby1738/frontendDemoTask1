709f3583c01f57a95349f567a37f993357dc3d1e
Ebenezer <ebenirene5@gmail.com>
2023-09-09 18:24:07 +0100

    made some correction to suit the test id

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
+// Function to update current day and time
+function updateDateTime() {
+    const currentDayElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+    const currentTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
 
-        // Function to update current day and time
-        function updateDateTime() {
-            const currentDayElement = document.getElementById("currentDayOfTheWeek");
-            const currentTimeElement = document.getElementById("currentUTCTime");
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const now = new Date();
 
-            const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-            const now = new Date();
+    const currentDay = daysOfWeek[now.getUTCDay()];
+    const currentTime = now.toISOString().substr(11, 8);
 
-            const currentDay = daysOfWeek[now.getUTCDay()];
-            const currentTime = now.toISOString().substr(11, 8);
+    currentDayElement.textContent = `Current day of the week: ${currentDay}`;
+    currentTimeElement.textContent = `Current UTC time: ${currentTime}`;
+}
 
-            currentDayElement.textContent = `Current day of the week: ${currentDay}`;
-            currentTimeElement.textContent = `Current UTC time: ${currentTime}`;
-        }
+// Call the function to initially set the current day and time
+updateDateTime();
 
-        // Call the function to initially set the current day and time
-        updateDateTime();
-
-        // Update the current day and time every second
-        setInterval(updateDateTime, 1000);
-    
+// Update the current day and time every second
+setInterval(updateDateTime, 1000);
